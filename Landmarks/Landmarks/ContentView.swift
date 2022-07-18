76a0f446e54894d39f3664eca002354a6348c779
eunjoo-choi <epcej0020@gmail.com>
2022-07-18 23:53:50 +0900

    SwiftUI text view 사용해보기
    - 프로젝트 생성
    - gitignore 작성 (잘 된건지 확인 필요)
    - textView 코드 사용

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by chenjoo on 2022/07/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Turtle Rock")
+            .font(.title)
+            .foregroundColor(.blue)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by chenjoo on 2022/07/18.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
