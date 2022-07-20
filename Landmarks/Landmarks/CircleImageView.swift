1777e624b57c53d0d8438be0d56ce23d994c023c
eunjoo-choi <epcej0020@gmail.com>
2022-07-21 00:06:48 +0900

    Removed file that shouldn't be tracked

---
+++ Landmarks/Landmarks/CircleImageView.swift
@@ -0,0 +1,20 @@
+//
+//  CircleImageView.swift
+//  Landmarks
+//
+//  Created by 최은주 on 2022/07/20.
+//
+
+import SwiftUI
+
+struct CircleImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImageView()
+    }
+}
