040557d3a854465a6eaa983e79ae10fc9155f7be
eunjoo-choi <epcej0020@gmail.com>
2022-07-21 00:07:05 +0900

    Image View 사용해보기

--- Landmarks/Landmarks/CircleImageView.swift
+++ Landmarks/Landmarks/CircleImageView.swift
@@ -2,14 +2,19 @@
 //  CircleImageView.swift
 //  Landmarks
 //
-//  Created by 최은주 on 2022/07/20.
+//  Created by chenjoo on 2022/07/20.
 //
 
 import SwiftUI
 
 struct CircleImageView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
     }
 }
 
