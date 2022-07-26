302774214b356757ba08e00db4f5cf341989cb5d
eunjoo-choi <epcej0020@gmail.com>
2022-07-26 20:59:45 +0900

    row 만들기
    ## 수정사항
    - row에 필요한 데이터(json, png) 파일 추가
    - row 구성

---
+++ Landmarks/Landmarks/Models/Landmark.swift
@@ -0,0 +1,35 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by 최은주 on 2022/07/26.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
